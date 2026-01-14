e2998a2a91391149a151fb85da91e893e8aa652f
Kieran McNay <kieran.mcnay1@engineering.digital.dwp.gov.uk>
2026-01-14 11:36:57 +0000

    Initial screen test for children tree
    GitOrigin-RevId: b554c2be9cb009510c5d102d9e2c31267224689f

--- app/routes.js
+++ app/routes.js
@@ -49,88 +49,20 @@ router.use('/', (req, res, next) => {
   // Add your routes here
 
   router.post(
-  '/pages/report-change/shared-care/shared-care-P3',
+  '/pages/report-change/children-tree',
   function (req, res) {
-    var numberOfNights = req.body['numberOfNights']
+    var ctWhichChild = req.body['childrentree-whichChild']
 
-    if (numberOfNights == "Yes"){
+    if (ctWhichChild == "New"){
   
-      res.redirect('/pages/report-change/shared-care/shared-care-P4')
+      res.redirect('/pages/report-change/children-tree/ct-whichParent')
       
-    } else if (numberOfNights == "No") {
+    } else if (ctWhichChild == "Financialsupport") {
       
-      res.redirect('/pages/report-change/shared-care/shared-care-rejection')
+      res.redirect('/pages/report-change/children-tree/ct-financiallySupports')
    
     } else {
-        res.redirect('/pages/report-change/shared-care/shared-care-P3')
+        res.redirect('/pages/report-change/children-tree/ct-whatToReport')
     }
   });
-
-    router.post(
-        '/pages/report-change/shared-care/shared-care-declaration',
-        function (req, res) {
-      
-          switch (req.session.data.writtenAgreement) {
-            case 'A current court order':
-                res.redirect('/pages/report-change/shared-care/shared-care-confirmation-evidence')
-              break;
-              case 'A formal agreement, for example drawn up by a solicitor':
-                res.redirect('/pages/report-change/shared-care/shared-care-confirmation-evidence')
-              break;
-              case 'Another official document, for example Children and Family Court Advisory and Support Service (CAFCASS) or Social Services reports':
-                res.redirect('/pages/report-change/shared-care/shared-care-confirmation-evidence')
-              break;
-            case 'None':
-              res.redirect('/pages/report-change/shared-care/shared-care-confirmation-no-evidence');
-              break;
-            default: 
-              res.redirect('/pages/report-change/shared-care/shared-care-declaration');
-              break;
-          }
-        }
-      );
-
-      router.post(
-        '/pages/report-change/shared-care/corroboration-declaration',
-        function (req, res) {
-      
-          switch (req.session.data.writtenAgreement) {
-            case 'A current court order':
-                res.redirect('/pages/report-change/shared-care/corroboration-confirmation')
-              break;
-              case 'A formal agreement, for example drawn up by a solicitor':
-                res.redirect('/pages/report-change/shared-care/corroboration-confirmation')
-              break;
-              case 'Another official document, for example Children and Family Court Advisory and Support Service (CAFCASS) or Social Services reports':
-                res.redirect('/pages/report-change/shared-care/corroboration-confirmation')
-              break;
-            case 'None':
-              res.redirect('/pages/report-change/shared-care/corroboration-confirmation-no-evidence');
-              break;
-            default: 
-              res.redirect('/pages/report-change/shared-care/corroboration-declaration');
-              break;
-          }
-        }
-      );
-
-    router.post(
-        '/pages/report-change/shared-care/corroboration-start',
-        function (req, res) {
-          var corroborationAgree = req.body['corroborationAgree']
-      
-          if (corroborationAgree == "Yes" ){
-        
-            res.redirect('/pages/report-change/shared-care/corroboration-CA-yes')
-            
-          } else if (corroborationAgree == "No") {
-            
-            res.redirect('/pages/report-change/shared-care/corroboration-P1')
-         
-          } else {
-              res.redirect('/pages/report-change/shared-care/corroboration-start')
-          }
-        });
-
-  
     
